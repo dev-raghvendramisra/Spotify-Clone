dde324c291d0dbeaed4c7360cf93e55c9290115a
dev-raghvendramisra <itsraghav12@gmail.com>
2024-03-02 21:12:48 +0530

    updated navigation.js

--- navigation.js
+++ navigation.js
@@ -1,37 +1,32 @@
-const searchTabBtn=document.querySelector("#searchicon");
-const homeTabBtn=document.querySelector("#homeicon");
+const searchTabBtn = document.querySelector("#searchicon");
+const homeTabBtn = document.querySelector("#homeicon");
 let stylingForTabs = document.createElement("style");
 document.querySelector("head").append(stylingForTabs);
 
-function tabUpdateForSearch(){
-    stylingForTabs.innerText="";
-    homeTabContainer.style.display="none";
-    searchTabContainer.style.display="initial";
-    mainWindow.style.backgroundImage=mainWindowBg[5];
-    searchTabBtn.querySelector("span").style.color="white";
-    searchTabBtn.querySelector("p").style.color="white";
-    stylingForTabs.innerText=stylingForSearchTab;
-
+function tabUpdateForSearch() {
+  stylingForTabs.innerText = "";
+  homeTabContainer.style.display = "none";
+  searchTabContainer.style.display = "initial";
+  mainWindow.style.backgroundImage = mainWindowBg[5];
+  searchTabBtn.querySelector("span").style.color = "white";
+  searchTabBtn.querySelector("p").style.color = "white";
+  stylingForTabs.innerText = stylingForSearchTab;
 }
-function tabUpdateForHome(){
-    stylingForTabs.innerText="";
-   homeTabContainer.style.display="initial";
-   searchTabContainer.style.display="none";
-    searchTabBtn.querySelector("span").style.color=" grey";
-    searchTabBtn.querySelector("p").style.color=" grey";
-    stylingForTabs.innerText=stylingForHomeTab;
-
+function tabUpdateForHome() {
+  stylingForTabs.innerText = "";
+  homeTabContainer.style.display = "initial";
+  searchTabContainer.style.display = "none";
+  searchTabBtn.querySelector("span").style.color = " grey";
+  searchTabBtn.querySelector("p").style.color = " grey";
+  stylingForTabs.innerText = stylingForHomeTab;
 }
 
-
-
-searchTabBtn.addEventListener("click",()=>{
-    tabUpdateForSearch();
-    history.pushState({},null,"/search");
+searchTabBtn.addEventListener("click", () => {
+  tabUpdateForSearch();
+  history.pushState({}, null, "/search");
 });
 
-homeTabBtn.addEventListener("click",()=>{
-    tabUpdateForHome();
-    history.pushState({},"","");
-
-})
+homeTabBtn.addEventListener("click", () => {
+  tabUpdateForHome();
+  history.pushState({}, "", "/");
+});
